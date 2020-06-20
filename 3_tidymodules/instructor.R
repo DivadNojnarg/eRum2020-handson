e78ea011af4afb43db33e5effac480165037e6f2
m-l-1 <larbaoui@gmail.com>
2020-06-20 11:38:14 +0000

    initial commit

---
+++ 3_tidymodules/instructor.R
@@ -0,0 +1,12 @@
+# Author : Mustapha Larbaoui
+# Email : mustapha.larbaoui@novartis.com
+# Company : Novartis
+
+
+# Chapter 1 : OOP & R6
+
+# Chapter 2 : Namespace management
+
+# Chapter 3 : Module communication
+
+# Chapter 4 & 5 : Inheritance & Session
