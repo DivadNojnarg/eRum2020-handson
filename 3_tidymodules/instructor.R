a6275df8649c2182a4bde60eca0841fdcf607be4
m-l-1 <larbaoui@gmail.com>
2020-06-20 13:45:24 +0000

    reformat file

--- 3_tidymodules/instructor.R
+++ 3_tidymodules/instructor.R
@@ -1,12 +1,20 @@
+# --------------------------------------
 # Author : Mustapha Larbaoui
 # Email : mustapha.larbaoui@novartis.com
 # Company : Novartis
+# --------------------------------------
 
 
 # Chapter 1 : OOP & R6
 
+## A. Do the short swirl lesson first
+## B. Copy the counter module example, adjust and convert it to {tm}
+
 # Chapter 2 : Namespace management
+
 
 # Chapter 3 : Module communication
 
+
 # Chapter 4 & 5 : Inheritance & Session
+
